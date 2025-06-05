8ad892112f71339c2129acbf308a8cc8d00eff0b
root <root@localhost.localdomain>
2025-06-05 13:32:23 +0100

    Update omv_tools.sh

--- omv_tools.sh
+++ omv_tools.sh
@@ -2,7 +2,7 @@
 
 # add basic aliases
 echo "PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] '" >> .bashrc
-echo "alias la='ls -lah'" >> ~/.bashrc
+echo "alias la='ls $LS_OPTIONS -lah'" >> ~/.bashrc
 echo "alias vpn_test='curl -sS https://am.i.mullvad.net/connected'" >> ~/.bashrc
 
 # disable motd
@@ -12,7 +12,12 @@ touch ~/.hushlogin
 wget -O - https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install | bash
 
 # safe tools to say yes to 
-apt install vim htop tmux neofetch curl -y
+apt install vim htop tmux neofetch curl man -y
+
+# add git st alias
+git config --global alias.st status
+# make vim default git editor
+git config --global core.editor "vim"
 
 # add neofetch to .profile
 echo "neofetch" >> ~/.profile
